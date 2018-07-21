026fa37e3d03e8194806af4705454fe18d034022
Santosh Dhanasure <santosh_dhanasure@persistent.co.in>
2018-07-21 15:42:33 +0530

    Adding one more line here to check conflict

--- index.php
+++ index.php
@@ -17,4 +17,4 @@ define('WP_USE_THEMES', true);
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
 // this is comming from 21july02 branch
-// Trying to merge the changes with master
+// Trying to merge the changes with master. Adding one more line here to check conflict
