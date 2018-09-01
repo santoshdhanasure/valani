0b5dda881861b5af84b0e4bf8b99ca8185340b75
Santosh Dhanasure <santosh_dhanasure@persistent.co.in>
2018-09-01 09:45:48 +0530

    resolving conflict while revert back

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -7,7 +7,7 @@
 
 // Im trying to understand diff when dev rebase with master
 
-if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) { // added comment to check if conflicts occures
+if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) { // what this line is
 	$protocol = $_SERVER['SERVER_PROTOCOL'];
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
 		$protocol = 'HTTP/1.0';
