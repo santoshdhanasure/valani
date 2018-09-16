4924b50a35f44c422642d4a89f3298cda00c868d
santoshdhanasure <santosh_dhanasure@persistent.co.in>
2018-09-16 21:31:06 +0530

    header and footer comment changed
    header and footer comment changed

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
+ 
+ First change
+ 
  */
 
 /**
@@ -18,3 +21,5 @@ require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
 // this is comming from 21july02 branch
 // Trying to merge the changes with master. Adding one more line here to check conflict
+
+// footer changed
